54f6319f51d8eb8d1bc9c5c4d61ad935e6c46553
josh-wilkinson <91944995+josh-wilkinson@users.noreply.github.com>
2023-10-22 20:05:20 +0100

    added comments

--- src/cm/Period.java
+++ src/cm/Period.java
@@ -10,10 +10,12 @@ public class Period
     }
     public int duration()
     {
+        // the duration method returns the duration of stay in hours.
         return 0;
     }
     public Boolean overlaps(Period period)
     {
+        // the overlaps method returns true if the passed 'Period' overlaps the current 'Period'.
         return true;
     }
 }
--- src/cm/Rate.java
+++ src/cm/Rate.java
@@ -21,6 +21,11 @@ public class Rate
     }
     public BigDecimal calculate(Period periodStay)
     {
+        /*
+         the calculate method returns the charge that a driver has to pay
+         for a stay in a car park of a particular kind. The length of
+         stay is defined by a Period (periodStay).
+        */
         return BigDecimal.valueOf(0);
     }
 }
