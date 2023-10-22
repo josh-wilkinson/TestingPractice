b412c8498437d2fe0021f19c1b9f1e071e69f8d9
josh-wilkinson <91944995+josh-wilkinson@users.noreply.github.com>
2023-10-22 18:26:47 +0100

    added some empty implementations of methods

--- src/cm/Period.java
+++ src/cm/Period.java
@@ -2,5 +2,18 @@ package cm;
 
 public class Period
 {
+    private int startHour;
+    private int endHour;
+    public Period(int start, int end)
+    {
 
+    }
+    public int duration()
+    {
+        return 0;
+    }
+    public Boolean overlaps()
+    {
+        return true;
+    }
 }
--- src/cm/Rate.java
+++ src/cm/Rate.java
@@ -1,6 +1,21 @@
 package cm;
 
+import java.math.BigDecimal;
+
 public class Rate
 {
+    enum CarParkKind
+    {
+        STAFF,
+        STUDENT,
+        MANAGEMENT,
+        VISITOR
+    }
+    CarParkKind kind;
+    BigDecimal hourlyNormalRate;
+    BigDecimal hourlyReducedRate;
+    public Rate()
+    {
 
+    }
 }
