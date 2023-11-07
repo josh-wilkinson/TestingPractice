cb1b1af84db033fc6cf42c7f1fc4b49c55b231b4
Joshua <joshwilk40@gmail.com>
2023-11-07 14:00:37 +0000

    changed 'startAndEndAreValid' test, added some more comments

--- src/cm/PeriodTest.java
+++ src/cm/PeriodTest.java
@@ -79,7 +79,7 @@ class PeriodTest
         });
     }
     // Valid Output
-    @Test   // BAD TEST
+    @Test   // this may need to be changed
     void startAndEndAreValid(){
         int start = 10;
         int end = 12;
@@ -94,13 +94,18 @@ class PeriodTest
     /**
      * Method 'duration()' tests
      */
-    // Valid outputs
-    @DisplayName("Test duration method")
+    /*
+     Valid outputs
+     Testing the boundaries between hours 0 and 24.
+     The shortest possible duration should be 1 hour, and the longest should be 24 hours.
+    */
+    @DisplayName("duration returns 1")
     @Test
     void durationReturnsOne(){
         Period p = new Period(0, 1);
         assertEquals(p.duration(), 1);
     }
+    @DisplayName("duration returns 24")
     @Test
     void durationReturnsTwentyFour(){
         Period p = new Period(0, 24);
@@ -110,38 +115,46 @@ class PeriodTest
     /**
       * Method 'overlaps(Period)' tests
      */
-    // Valid Outputs
-    @DisplayName("Test overlaps method")
+    /*
+     Valid Outputs
+     Here are all the different ways the method 'overlaps' can return TRUE or FALSE
+    */
+    @DisplayName("overlaps returns TRUE 1")
     @Test
     void overlapsReturnsTrue1(){
         Period oldPeriod = new Period(4, 8);
         Period period = new Period(6, 10);
         assertTrue(oldPeriod.overlaps(period));
     }
+    @DisplayName("overlaps returns TRUE 2")
     @Test
     void overlapsReturnsTrue2(){
         Period oldPeriod = new Period(8, 10);
         Period period = new Period(6, 9);
         assertTrue(oldPeriod.overlaps(period));
     }
+    @DisplayName("overlaps returns TRUE 3")
     @Test
     void overlapsReturnsTrue3(){
         Period oldPeriod = new Period(12, 16);
         Period period = new Period(10, 18);
         assertTrue(oldPeriod.overlaps(period));
     }
+    @DisplayName("overlaps returns TRUE 4")
     @Test
     void overlapsReturnsTrue4(){
         Period oldPeriod = new Period(15, 18);
         Period period = new Period(16, 17);
         assertTrue(oldPeriod.overlaps(period));
     }
+    @DisplayName("overlaps returns FALSE 1")
     @Test
     void overlapsReturnsFalse1(){
         Period oldPeriod = new Period(9, 10);
         Period period = new Period(11, 12);
         assertFalse(oldPeriod.overlaps(period));
     }
+    @DisplayName("overlaps returns FALSE 1")
     @Test
     void overlapsReturnsFalse2(){
         Period oldPeriod = new Period(14, 15);
