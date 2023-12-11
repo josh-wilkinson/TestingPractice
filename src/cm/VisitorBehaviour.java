9b0db37e6ac79614b7e1e2c2eee2e518a220e323
Joshua <joshwilk40@gmail.com>
2023-12-11 14:58:42 +0000

    refactored code to make sure the amount is rounded to 2 decimal places

--- src/cm/VisitorBehaviour.java
+++ src/cm/VisitorBehaviour.java
@@ -7,7 +7,10 @@ class VisitorBehaviour implements PaymentStrategy{
     public BigDecimal paymentBehaviour(BigDecimal amount) {
         if (amount.doubleValue() <= 10) // if the price is 10 or under
             return BigDecimal.valueOf(0);
-        else // subtract 10, then halve the price.
-            return amount.subtract(BigDecimal.valueOf(10)).multiply(BigDecimal.valueOf(0.5));
+        else { // subtract 10, then halve the price.
+            amount = amount.subtract(BigDecimal.valueOf(10)).multiply(BigDecimal.valueOf(0.5));
+            amount = amount.setScale(2, BigDecimal.ROUND_HALF_EVEN); // rounds amount to 2 decimal places
+            return amount;
+        }
     }
 }
