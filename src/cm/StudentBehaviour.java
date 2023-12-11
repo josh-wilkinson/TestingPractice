24131454d00b81732f3e5ed2b516a761d90dec5a
Joshua <joshwilk40@gmail.com>
2023-12-11 14:55:31 +0000

    made fix to StudentBehaviour to pass test 'studentGetsDiscount'

--- src/cm/StudentBehaviour.java
+++ src/cm/StudentBehaviour.java
@@ -5,9 +5,11 @@ import java.math.BigDecimal;
 class StudentBehaviour implements PaymentStrategy{
     @Override
     public BigDecimal paymentBehaviour(BigDecimal amount) {
-        if (amount.doubleValue() > 5.5) // if the price is above 5.50, take away 33%
-            return BigDecimal.valueOf(amount.doubleValue()).subtract(
-                    BigDecimal.valueOf(amount.doubleValue()).multiply(BigDecimal.valueOf(0.33)));
+        if (amount.doubleValue() > 5.5) { // if the price is above 5.50, take away 33% on any amount about it
+            amount = amount.subtract(BigDecimal.valueOf(amount.doubleValue()-5.50).multiply(BigDecimal.valueOf(0.33)));
+            amount = amount.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+            return amount;
+        }
         return amount;
     }
 }
